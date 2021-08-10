0fc248a7ec11a7ec12c80fc5b7b34b315b516a6f
Ernesto Soto Gómez <esto.yinyang@gmail.com>
2021-08-09 22:03:07 -0400

    Added doctests for lazy shuffle_repetition generation

--- pathpy/expressions/repetitions/shuffle_repetition.py
+++ pathpy/expressions/repetitions/shuffle_repetition.py
@@ -34,10 +34,10 @@ class ShuffleRepetition(Repetition):
         >>> from pathpy import Shuffle as S, Concatenation as C
 
         >>> exp = S('ab')%[1,2]
-        >>> assert exp.get_language() == {'ab', 'ba', 'abab', 'abba', 'baba', 'aabb', 'baab', 'bbaa'}
+        >>> assert exp.get_language() == exp.get_generator().get_language() == {'ab', 'ba', 'abab', 'abba', 'baba', 'aabb', 'baab', 'bbaa'}
 
         >>> exp &= C('ab')%...
-        >>> assert exp.get_language() == {'ab', 'abab', 'aabb'}
+        >>> assert exp.get_language() == exp.get_generator().get_language() == {'ab', 'abab', 'aabb'}
     """
 
 
