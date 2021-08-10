222a1ffbb938c0a041511a7f4e23544458059810
Ernesto Soto Gómez <esto.yinyang@gmail.com>
2021-08-09 22:05:07 -0400

    Added dostests for lazy LettersPossitiveUnion generation

--- pathpy/expressions/terms/letters_unions/letters_possitive_union.py
+++ pathpy/expressions/terms/letters_unions/letters_possitive_union.py
@@ -8,9 +8,11 @@ class LettersPossitiveUnion(LettersUnion):
     At the semantic level, an union of letters is the same as a union of expression, where each expression is a letter, but it is preferred to use LettersPossitiveUnion in this case.
 
     >>> from pathpy import LettersPossitiveUnion as Ls
-    >>> assert Ls('abcd').get_language() == {'a', 'b', 'c', 'd'}
+    >>> exp = Ls('abcd')
+    >>> assert exp.get_language() == exp.get_generator().get_language() == {'a', 'b', 'c', 'd'}
 
-    >>> assert (Ls('ab') | Ls('cd')).get_language() == {'a', 'b', 'c', 'd'}
+    >>> exp = Ls('ab') | Ls('cd')
+    >>> assert exp.get_language() == exp.get_generator().get_language() == {'a', 'b', 'c', 'd'}
     """
 
     def get_one_rest(self):
