3a09d68a793d38e469de42be5594ee7ebefcd640
Ernesto Soto Gómez <esto.yinyang@gmail.com>
2021-08-26 22:19:44 -0400

    Changed shuffle_visitor
    Previous version was recursive. Current version is iterative.

--- pathpy/generation/machines/visitors/shuffle_visitor.py
+++ pathpy/generation/machines/visitors/shuffle_visitor.py
@@ -1,21 +1,23 @@
-from .decorators import nary_operator_visitor
+from collections import deque
+
 from pathpy.expressions.nary_operators.shuffle import Shuffle
 from pathpy.expressions.terms.empty_word import EMPTY_WORD
 
 from ..machine import Branches, Machine
+from .decorators import nary_operator_visitor
 
 __all__ = ['shuffle_visitor']
 
-@nary_operator_visitor
-def shuffle_visitor(machine: Machine, input: Shuffle) -> Branches:
-    def shuffle(a, b):
-        for head, tail in machine.branches(a):
-            # aA // B = a + (A // B) | ...
-            if tail is not EMPTY_WORD:
-                yield head, Shuffle(tail, b)
-            else:
-                # a // B = a + B | ...
-                yield head, b
 
-    yield from shuffle(input.head, input.tail)
-    yield from shuffle(input.tail, input.head)
+@nary_operator_visitor
+def shuffle_visitor(machine: Machine, exp: Shuffle) -> Branches:
+    front = deque()
+    back = deque(exp.arguments)
+    while back:
+        e = back.popleft()
+        for head, tail in machine.branches(e):
+            if head is not EMPTY_WORD:
+                rest = Shuffle(front + back)
+                tail = rest if tail is EMPTY_WORD else Shuffle(tail, rest)
+                yield head, tail
+        front.append(e)
