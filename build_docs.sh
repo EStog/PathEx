633721c379f21f0012a48b14fe88e12d4a9cf1cb
Ernesto Soto Gómez <esto.yinyang@gmail.com>
2021-09-19 13:53:53 -0400

    update build and test scripts

--- build_docs.sh
+++ build_docs.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR="$( dirname "${BASH_SOURCE[0]}" )"
 
 echo building docs...
 sphinx-apidoc -f -E -e -M -o ./docs/source/API ./pathex
-cd ./${SCRIPT_DIR0}/docs
+cd ./${SCRIPT_DIR}/docs
 make $@
--- run_tests.sh
+++ run_tests.sh
@@ -6,5 +6,6 @@ pytest --doctest-modules --cov= --cov-report=xml
 SCRIPT_DIR="$( dirname "${BASH_SOURCE[0]}" )"
 
 echo running tests to docs...
-cd ./${SCRIPT_DIR0}/docs
+sphinx-apidoc -f -E -e -M -o ./docs/source/API ./pathex
+cd ./${SCRIPT_DIR}/docs
 make doctest
